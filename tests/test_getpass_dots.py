712e65acfc19f960ea1b05bc9bcb4b254387ba19
Andy Brody <git@abrody.com>
2025-01-18 21:37:12 -0500

    Add tests of top level module.

--- tests/test_getpass_dots.py
+++ tests/test_getpass_dots.py
@@ -1,6 +1,23 @@
+import sys
+
+import pytest
+
 from getpass_dots import Getpass, getpass_dots
 from getpass_dots.abstract_getpass import AbstractGetpass
+from getpass_dots.unix_getpass import UnixGetpass
 
 
 def test_getpass_class():
     assert issubclass(Getpass, AbstractGetpass)
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on Windows")
+def test_getpass_unix():
+    assert Getpass == UnixGetpass
+
+
+@pytest.mark.skipif(sys.platform != "win32", reason="only runs on Windows")
+def test_getpass_windows():
+    from getpass_dots.windows_getpass import WindowsGetpass
+
+    assert Getpass == WindowsGetpass
